ef7b68020c4fa880f63d8f06b5dd0eb5716bd264
aaron-smth <s1155092086@gmail.com>
2018-01-27 13:53:52 +0800

    revising logger

--- lab1/tools/logger.py
+++ lab1/tools/logger.py
@@ -1,8 +1,27 @@
 import logging 
+import re
+
+fname = 'results.log'
 
 LOG_FORMAT = '%(asctime)s - %(message)s'
-logging.basicConfig(filename='results.log', level=logging.DEBUG, format=LOG_FORMAT)
+logging.basicConfig(filename=fname, level=logging.DEBUG, format=LOG_FORMAT)
 logger = logging.getLogger()
+
+
+def logging_number():
+    '''Writing head: Experiment []'''
+    with open(fname, 'r') as f:
+        content = f.readlines()
+    if len(content) <= 1:
+        logger.info('Experiment 1')
+    else:
+        pattern = 'Experiment (\d+)'
+        for line in reversed(content):
+            result = re.search(pattern, line)
+            if result:
+                n = result.group(1)
+                logger.info('Experiment {}'.format(int(n)+1))
+
 
 def logging_dict(d):
     import sys
@@ -12,7 +31,9 @@ def logging_dict(d):
     logger.info(', '.join([message.format(*tup) for tup in li]))
     print('Results Logged')
 
+
 def logging_time(func):
+    '''Recording time'''
     import time 
     def wrapper(*args, **kwargs):
         start = time.time()
