9ed56d94a718745f436bc8fb7aacbb58e60bcea5
aaron-smth <s1155092086@gmail.com>
2018-01-27 13:17:20 +0800

    revising RNG

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-from tools.logger import logging_time, logging_dict
+from tools.logger import logging_time, logging_dict, logging_number
 
+logging_number()
 def LCG():
     '''LCG Random Number generator'''
     # seed, a, c, m are all prime numbers, to avoid periodicity
@@ -11,28 +12,40 @@ def LCG():
     a = 1559
     c = 313
     m = 13229
+
+    logging_dict(locals())
+
     while True:
-        if state / m < 0.5:
-            yield -1
-        else:
-            yield 1
+        yield state / m 
         state = (a*state+c) % m
 
-def npRNG():
-    '''Numpy Random Number Generator'''
-    while True:
-        if np.random.random() < 0.5:
-            yield -1
-        else:
-            yield 1
-#instantiate the generators LCG and npRNG
+#instantiate the LCG
 LCG = LCG()
-npRNG = npRNG()
+
+def random_choice(choices, size=1):
+
+    def hash(p):
+        if type(choices) == int:
+            return int(p * choices)
+        else:
+            return choices[int(p * len(choices))]
+    vhash = np.vectorize(hash) 
+    
+    random_ns = np.fromiter(LCG, dtype=float, count=size)
+    return vhash(random_ns) 
 
 
-def RandomWalk(steps, RNG): 
-    walks = list(next(RNG) for i in range(steps*3))
-    walks = np.array(walks).reshape(-1,3)
+def RandomWalk(steps, RNG='npRNG'): 
+    walks = np.zeros((steps,3))
+    if RNG=='LCG':
+        dims = random_choice(3, size=steps)
+        ahead = random_choice([-1,1], size=steps)
+    elif RNG=='npRNG':
+        dims = np.random.choice(3, size=steps)
+        ahead = np.random.choice([-1,1], size=steps)
+    
+    walks[np.arange(steps), dims] = ahead
+
     pos = np.array(walks).cumsum(axis=0) # cumulative sum of walks in every step
     return pos
 
@@ -54,14 +67,14 @@ def samppling(N, walk_steps, sample_steps, RNG):
     return sample_indices, MSD_mean
 
 @logging_time #recording the runtime of this function and recording it in results.log
-def linearity_test(RNG, ax, name=None):
+def linearity_test(RNG, ax):
     '''1.Generate a 500 particle system, each walking 5000 steps, sampled every 10 steps
        2.Finding the MSD of this system
        3.Linear fit the function (N,MSD) using scipy.optimize.curve_fit
        4.Plotting the fitting line and the data'''
  
     print('Linear fitting of random walk')
-    print('RNG: {}'.format(name))
+    print('RNG: {}'.format(RNG))
     xs, ys = samppling(500, 5000, 10, RNG)
 
     def f(x, slope):
@@ -78,7 +91,7 @@ def linearity_test(RNG, ax, name=None):
  
     logging_dict(locals()) #recording the results of this test 
 
-    ax.set(title= name, xlabel='steps', ylabel='distance squared')
+    ax.set(title= RNG, xlabel='steps', ylabel='distance squared')
     ax.plot(xs, ys)
     ax.plot(xs, yfit, color='red', alpha=0.7, label='linear fitting')
 
@@ -86,16 +99,16 @@ def linearity_test(RNG, ax, name=None):
 def plot_linear():
     '''Plotting the two test together'''
     fig, axes = plt.subplots(nrows=1,ncols=2, sharey=True, figsize=(12, 4))
-    linearity_test(LCG, axes[0], 'LCG')
-    linearity_test(npRNG, axes[1], 'numpy RNG')
+    linearity_test('LCG' ,axes[0] )
+    linearity_test('npRNG', axes[1], )
     plt.show(fig)
     print('The seed is predetermined for function LCG, so the slope of LCG is the same everytime, but npRNG changes everytime you run')
 
 # writting the XYZ file
 def write_to_XYZ():
     steps = 5000
-    LCG_walk = RandomWalk(steps, LCG)
-    npRNG_walk = RandomWalk(steps, npRNG)
+    LCG_walk = RandomWalk(steps, 'LCG')
+    npRNG_walk = RandomWalk(steps, 'npRNG')
     from tools.XYZ_format import write_XYZ
     write_XYZ('LCG.xyz', 'a random walk of 5000 steps using LCG random number generator', LCG_walk)
     write_XYZ('npRNG.xyz', 'a random walk of 5000 steps using numpy random number generator', npRNG_walk)
