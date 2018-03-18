ffcdb542333de4a2613a53beca20e6fa4267a666
Aaron <aaron@aaron-macpro.local>
2018-03-19 02:23:33 +0800

    17 times faster!

--- graphene/tersoff.py
+++ graphene/tersoff.py
@@ -23,36 +23,57 @@ g_theta = lambda Ctheta: 1+ c**2 * ( 1/d**2 - 1/(d**2 + (h - Ctheta)**2) )
 b_eta = lambda eta: (1 + (beta * eta)**n) ** (-1/(2*n))  
 
 
-def g_func(rj, dj):
-    natoms = len(rj)
-    dotjk = np.einsum('jd,kd->jk', rj, rj) #j,k
-    norm = np.einsum('j,k->jk', dj, dj)
-    norm += np.identity(natoms)
-    Ctheta = dotjk / norm  
+def g_func(rij, dij):
+    dotijk = np.einsum('ijd,ikd->ijk', rij, rij) #j,k
+    normijk = np.einsum('ij,ik->ijk', dij, dij)
+    np.seterr(divide='ignore')
+    Ctheta = dotijk / normijk
     return g_theta(Ctheta)
 
-def b_func(rj, dj, cutj):
-    etajk = np.einsum('k, jk->jk', cutj, g_func(rj, dj) ) 
-    etaj = np.sum(etajk, axis=1) - np.diag(etajk)
-    return b_eta(etaj)
+def b_func(rij, dij, cutij):
+    etaijk = np.einsum('ij, ijk->ijk', cutij, g_func(rij, dij) ) 
+    s0,s1,s2 = etaijk.shape
+    etaij = np.sum(etaijk, axis=1) - etaijk.reshape(s0,-1)[:,::s2+1] #diagonal
+    return b_eta(etaij)
+
+def group_by_neighbors(cut_groups):
+    length_d = {}
+    for neighbors in cut_groups:
+        N = neighbors.size 
+        if N not in length_d:
+            length_d[N] = neighbors[None, :]
+        else:
+            length_d[N] = np.vstack((length_d[N], neighbors))
+    return length_d
 
 def neighborV_gen(rjN, djN, cut_groups):
     cutj =  np.where( djN>R-D, cut_off(djN), 1)
-    fRj = fR(djN)
+
+    yield np.sum( cutj * fR(djN) )
+
     fAj = fA(djN)
-    for js in cut_groups:
-        if len(js) == 1:
-            bj = 1
+    length_d = group_by_neighbors(cut_groups)
+    for N,inds in length_d.items(): 
+        if N == 1:    
+            ind1 = inds[:, 0]
+            yield np.sum( -cutj[ind1] * fAj[ind1] ) 
         else:
-            bj = b_func(rjN[js], djN[js], cutj[js])  
-        yield np.sum( cutj[js] * (fRj[js] - bj * fAj[js]) ) 
+            bij = b_func(rjN[inds], djN[inds], cutj[inds])  
+            yield np.sum( cutj[inds] * bij * -fAj[inds])
+        
+    #for js in cut_groups:
+    #    if len(js) == 1:
+    #        bj = 1
+    #    else:
+    #        bj = b_func(rjN[js], djN[js], cutj[js])  
+    #    yield np.sum( -cutj[js] *  bj * fAj[js]) 
         
 def tersoff_V(pos):
     natoms = len(pos) 
 
     rij = pos[None, :] - pos[:, None] + np.identity(natoms)[:,:,None]
     dij =  np.linalg.norm( rij, axis=2 )
-    dij += np.identity(natoms)*2*R
+    dij+=  np.identity(natoms) * (2 * R)
 
     cut_mask = dij < R+D
     if not cut_mask.any(): return 0.
